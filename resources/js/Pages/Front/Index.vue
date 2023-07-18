cc818c10ac542099c01cd2bb8f1368688da9c4fb
21.11.4346_Ahmad <101561847+ahmdfauzzan@users.noreply.github.com>
2023-07-18 21:18:18 +0700

    Update Index.vue

--- resources/js/Pages/Front/Index.vue
+++ resources/js/Pages/Front/Index.vue
@@ -58,7 +58,7 @@ const props = defineProps({
                 <img src="img/logo_Web.png" class="h-8 mr-3" alt=" Logo" />
                 <span
                     class="self-center text-2xl font-semibold whitespace-nowrap dark:text-dark"
-                    >Dosen AI</span
+                    >Dosen Fuck</span
                 >
             </a>
             <div class="flex md:order-2">
